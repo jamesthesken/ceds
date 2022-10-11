5f1ef25c4a55cb9a64f000dc90b50629156675c9
jamesthesken <jthesken@hawaii.edu>
2022-10-10 15:37:50 -1000

    chore: clean up imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,4 @@
 import type { NextPage } from 'next'
-import { Fragment, useState } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
-import {
-  Bars3Icon,
-  CalendarIcon,
-  ChartBarIcon,
-  FolderIcon,
-  HomeIcon,
-  InboxIcon,
-  UsersIcon,
-  XMarkIcon,
-} from '@heroicons/react/24/outline'
 import QuickFacts from './components/QuickFacts'
 import Layout from './components/Layout'
 
